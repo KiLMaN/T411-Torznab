50fde3dd519fa60a970bfe4b30a310f5f2663f2d
KiLMaN <bricegdu95@hotmail.fr>
2015-09-14 16:31:15 +0200

    Update to use TvMaze instead of TvRage

--- server.js
+++ server.js
@@ -6,10 +6,12 @@ var applicationPort = config.listenPort;
 
 // Require 
 var express = require ('express');
-var tvrage = require('tvragejson');
+//var tvrage = require('tvragejson');
 var request = require ('request');
 var xml = require('xml');
 var NodeCache = require('node-cache');
+var tvmaze = require('./tvmaze.js');
+
 
 // Configuration
 var DEFAULT_LIMIT = 50;
@@ -27,10 +29,10 @@ function _TorznabServerPresentation(res)
 {
 	var xmlString = xml({
 		'caps':
-		[
+			[
 			{'server':{_attr:{'version':'1.0','title':'T411 Torznab','image':'http://www.t411.io/themes/blue/images/logo.png'}}}, 
-			{'limits':{_attr:{'max':'100','default':DEFAULT_LIMIT}}},
-			{'registration':{_attr:{'available':'no','open':'no'}}},
+		{'limits':{_attr:{'max':'100','default':DEFAULT_LIMIT}}},
+		{'registration':{_attr:{'available':'no','open':'no'}}},
 			{'searching':[
 				{'search':{_attr:{'available':'yes'}}},
 				{'tv-search':{_attr:{'available':'yes'}}},
@@ -39,14 +41,14 @@ function _TorznabServerPresentation(res)
 			{'categories':[
 				{'category':{_attr:{'id':'433','name':'Séries TV'}}},
 			]}
-		]
+			]
 	});
 	res.contentType('text/xml');
 	res.send('<?xml version="1.0" encoding="utf-8"?>\n' + xmlString);
 }
 function _TorznabSearchGlobal()
 {
-	
+
 }
 function reponseSearch(error, response, body)
 {
@@ -59,9 +61,9 @@ function reponseSearch(error, response, body)
 			// Remove <div> warnings if they exists
 			if(body.indexOf("</div>") != -1)
 				body = body.substring(body.lastIndexOf("</div>") + 6 );
-			
+
 			var jsonResp = JSON.parse (body); // Parse The JSON
-			
+
 			var torrentList = [];
 			if(jsonResp.torrents != undefined)
 			{
@@ -79,34 +81,34 @@ function reponseSearch(error, response, body)
 				console.log("Got "+torrentList.length+" torrents");	
 				xmlString = xml({
 					'rss':
-					[	
-						
+						[	
+
 						{_attr:{'version':'2.0','xmlns:torznab':'http://torznab.com/schemas/2015/feed'}},
-						{'channel':
+					{'channel':
 						[
-							{'title':'T411 Torznab'}, 
-							{'description':'T411 Torznab search result for '+jsonResp.query}, 
-							{'torznab:response':{_attr:{'offset':jsonResp.offset,'total':jsonResp.total}}}, 
-							
+						{'title':'T411 Torznab'}, 
+						{'description':'T411 Torznab search result for '+jsonResp.query}, 
+						{'torznab:response':{_attr:{'offset':jsonResp.offset,'total':jsonResp.total}}}, 
+
 						].concat(torrentList)
-						}
-					]
+					}
+						]
 				});
 			}
 			else
 			{
 				xmlString = xml({
 					'rss':
-					[
+						[
 						{_attr:{'version':'2.0','xmlns:torznab':'http://torznab.com/schemas/2015/feed'}},
 						{'channel':
-						[
+							[
 							{'title':'T411 Torznab'}, 
 							{'description':'T411 Torznab search result'}, 
 							{'torznab:response':{_attr:{'offset':'0','total':'0'}}}
-						]
+							]
 						}
-					]
+						]
 				});
 			}
 		}
@@ -115,16 +117,16 @@ function reponseSearch(error, response, body)
 			console.log("Exception : "+e);
 			xmlString = xml({
 				'rss':
-				[
+					[
 					{_attr:{'version':'2.0','xmlns:torznab':'http://torznab.com/schemas/2015/feed'}},
 					{'channel':
-					[
+						[
 						{'title':'T411 Torznab'}, 
 						{'description':'T411 Torznab search result'}, 
 						{'torznab:response':{_attr:{'offset':'0','total':'0'}}}
-					]
+						]
 					}
-				]
+					]
 			});
 		}
 		this.res.contentType('text/xml');
@@ -149,142 +151,155 @@ function research(urlSearch,callback,originalQuery)
 
 // Map URL
 app.get ('/api', function (req, res)
-{
-	var thisHostName = (req.headers['x-forwarded-host']) ? (req.headers['x-forwarded-host'] ) : ('127.0.0.1:' + applicationPort);
-	thisHostName = 'http://' + thisHostName;
-	
-	if(req.query.t && req.query.t == 'caps')
-	{
-		_TorznabServerPresentation(res);
-		return;
-	}
-	else if(req.query.t && req.query.t == 'search')
-	{
-		var query =(req.query.q) ? req.query.q : "";
-		research( baseUrl + "/torrents/search/"+query ,reponseSearch.bind( {res: res,hostname:thisHostName} ),req.query);
-	}
-	else if(req.query.t && req.query.t == 'tvsearch')
-	{						
-		var query = (req.query.q) ? req.query.q : "";
-		if(req.query.rid)
 		{
-				function researchTvRage(show) {
-					var showName = show['Showinfo']['showname'];
-					console.log("Researching for :" + showName);
-					showName = showName.replace(/\([0-9]*\)/gmi, "").trim();
-					showName = showName.replace(/\((US|FR|ES)\)/gmi, "").trim();
-					showName = showName.replace(/['|"](s)*/gmi, "").trim();
-					console.log("Clean Name : "+showName);
-					if (req.query.ep) { //This is an episode
-						query = showName + ' S' + pad(+req.query.season, 2) + 'E' + pad(+req.query.ep, 2);
+			var thisHostName = (req.headers['x-forwarded-host']) ? (req.headers['x-forwarded-host'] ) : ('127.0.0.1:' + applicationPort);
+			thisHostName = 'http://' + thisHostName;
+
+			if(req.query.t && req.query.t == 'caps')
+			{
+				_TorznabServerPresentation(res);
+				return;
+			}
+			else if(req.query.t && req.query.t == 'search')
+			{
+				var query =(req.query.q) ? req.query.q : "";
+				research( baseUrl + "/torrents/search/"+query ,reponseSearch.bind( {res: res,hostname:thisHostName} ),req.query);
+			}
+			else if(req.query.t && req.query.t == 'tvsearch')
+			{						
+				var query = (req.query.q) ? req.query.q : "";
+				if(req.query.rid)
+				{
+					function researchTvRage(show) {
+						console.log(show);
+						var showName = show['Showinfo']['showname'];
+						console.log("Researching for :" + showName);
+						showName = showName.replace(/\([0-9]*\)/gmi, "").trim();
+						showName = showName.replace(/\((US|FR|ES)\)/gmi, "").trim();
+						showName = showName.replace(/['|"](s)*/gmi, "").trim();
+						console.log("Clean Name : "+showName);
+						if (req.query.ep) { //This is an episode
+							query = showName + ' S' + pad(req.query.season, 2) + 'E' + pad(+req.query.ep, 2);
+						}
+						else { //This is a season
+							query = showName + ' S' + pad(req.query.season,2);
+						}
+						research( baseUrl + "/torrents/search/"+query ,reponseSearch.bind( {res: res,hostname:thisHostName} ),req.query);
 					}
-					else { //This is a season
-						query = showName + ' ' + req.query.season;
+					function tvRageResult(err,show)
+					{
+						if(err)
+						{
+							console.log("TvRage Error : "+err);
+							tvmaze.showInfoTvRage(req.query.rid, tvRageResult);
+						}
+						else
+						{
+							console.log("Got informations from TvRage");
+							tvRageCache.set(req.query.rid,show);
+							researchTvRage(show);
+						}
+					}
+					var cachedShow = tvRageCache.get(req.query.rid);
+					if(cachedShow == undefined)
+					{
+						console.log("TvRage Cache for "+req.query.rid+" empty, querying tvRage");
+						tvmaze.showInfoTvRage(req.query.rid,tvRageResult);
+						//tvrage.showInfo(req.query.rid,tvRageResult);
+					}
+					else 
+					{
+						console.log("TvRage Cache hit for " +req.query.rid);
+						researchTvRage(cachedShow);
+					}
+				}
+				else
+				{
+
+					if( req.query.season)
+					{
+						if (req.query.ep) { //This is an episode
+							query += ' S' + pad(req.query.season, 2) + 'E' + pad(+req.query.ep, 2);
+						}
+						else { //This is a season
+							query += ' S' + pad(req.query.season,2);
+						}
 					}
 					research( baseUrl + "/torrents/search/"+query ,reponseSearch.bind( {res: res,hostname:thisHostName} ),req.query);
 				}
-				function tvRageResult(err,show)
-				{
-					if(err)
-					{
-						console.log("TvRage Error : "+err);
-						tvrage.showInfo(req.query.rid, tvRageResult);
-					}
-					else
-					{
-						console.log("Got informations from TvRage");
-						tvRageCache.set(req.query.rid,show);
-						researchTvRage(show);
-					}
-				}
-				var cachedShow = tvRageCache.get(req.query.rid);
-				if(cachedShow == undefined)
-				{
-					console.log("TvRage Cache for "+req.query.rid+" empty, querying tvRage");
-					tvrage.showInfo(req.query.rid,tvRageResult);
-				}
-				else 
-				{
-					console.log("TvRage Cache hit for " +req.query.rid);
-					researchTvRage(cachedShow);
-				}
-		}
-		else
-			research( baseUrl + "/torrents/search/" ,reponseSearch.bind( {res: res,hostname:thisHostName} ),req.query);
-		
-	}
-	else
-	{
+			}
+			else
+			{
 
-	}
-});
+			}
+		});
 
 app.get ('/torrent/:torrentid',
-	function (req, res){
-		console.log ("get torrent : " + req.params.torrentid  );
-		if (!isNaN (req.params.torrentid) && req.params.torrentid != 0)
-		{
-			var requestData = {
-				url: baseUrl +  "/torrents/download/" + req.params.torrentid,
-				headers:{
-					'Authorization':userToken
-				}
-			}; 
-			function callBackTorrent (error, response, body)
+		function (req, res){
+			console.log ("get torrent : " + req.params.torrentid  );
+			if (!isNaN (req.params.torrentid) && req.params.torrentid != 0)
 			{
-				if (!error && response.statusCode == 200)
+				var requestData = {
+					url: baseUrl +  "/torrents/download/" + req.params.torrentid,
+					headers:{
+						'Authorization':userToken
+					}
+				}; 
+				function callBackTorrent (error, response, body)
 				{
-					res.contentType('application/x-bittorrent');
-					res.setHeader('Content-Disposition','attachment; filename="'+req.params.torrentid+'.torrent"');
-					res.write(body);
+					if (!error && response.statusCode == 200)
+					{
+						res.contentType('application/x-bittorrent');
+						res.setHeader('Content-Disposition','attachment; filename="'+req.params.torrentid+'.torrent"');
+						res.write(body);
+					}
 				}
+				request (requestData).pipe(res);	
 			}
-			request (requestData).pipe(res);	
+			else
+			{
+				res.contentType ('text/plain');
+				res.status (404); 
+				res.send ("Torrent not found");
+			}
 		}
-		else
-		{
-			res.contentType ('text/plain');
-			res.status (404); 
-			res.send ("Torrent not found");
-		}
-	}
 );
 function pad(n, width, z) {
-    z = z || '0';
-    n = n + '';
-    return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+	z = z || '0';
+	n = n + '';
+	return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
 }
 function _toTorznabElement (torrent,currentHostname)
 {
 	return {
 		'item':
-		[	
+			[	
 			{title: torrent.name},
 			{guid: torrent.id}, 
 			{enclosure:{_attr:{
-				'url': currentHostname+'/torrent/'+torrent.id,
-				'length': 500000000, 
-				'type':'application/x-bittorrent'
-				}}	
+						  'url': currentHostname+'/torrent/'+torrent.id,
+						  'length': 500000000, 
+						  'type':'application/x-bittorrent'
+					  }}	
 			},
-			{link: currentHostname+'/torrent/'+torrent.id}, 
-			/*{custom_elements:[*/
-			{'description':torrent.name},
-			{'pubDate':(new Date (torrent.added).toGMTString ())},
-			{'comments': baset411+'/t/'+torrent.id},
-			//{'category': 'HDTV 1080p'},
-			//{'link':''},
-			//{'torznab:attr': { _attr: { name: 'rageid', value: '37780'}}},
-			//{'torznab:attr':{_attr:{name: 'infohash'		, value:torrent['showrss:info_hash']['#']}}},
-			//{'torznab:attr':{_attr:{name: 'magneturl'		, value:torrent.link}}},
-			{'torznab:attr':{_attr:{name: 'seeders'			, value:torrent.seeders}}},
-			{'torznab:attr':{_attr:{name: 'leechers'		, value:torrent.leechers}}},
-			//{'torznab:attr':{_attr:{name: 'minimumratio'	, value:'0.0'}}},
-			//{'torznab:attr':{_attr:{name: 'minimumseedtime'	, value:'0.0'}}},
-			{'torznab:attr':{_attr:{name: 'size'			, value:torrent.size}}},
-			/*	]
-			}*/
-		]
+				{link: currentHostname+'/torrent/'+torrent.id}, 
+				/*{custom_elements:[*/
+				{'description':torrent.name},
+					{'pubDate':(new Date (torrent.added).toGMTString ())},
+					{'comments': baset411+'/t/'+torrent.id},
+						//{'category': 'HDTV 1080p'},
+						//{'link':''},
+						//{'torznab:attr': { _attr: { name: 'rageid', value: '37780'}}},
+						//{'torznab:attr':{_attr:{name: 'infohash'		, value:torrent['showrss:info_hash']['#']}}},
+						//{'torznab:attr':{_attr:{name: 'magneturl'		, value:torrent.link}}},
+						{'torznab:attr':{_attr:{name: 'seeders'			, value:torrent.seeders}}},
+						{'torznab:attr':{_attr:{name: 'leechers'		, value:torrent.leechers}}},
+							//{'torznab:attr':{_attr:{name: 'minimumratio'	, value:'0.0'}}},
+							//{'torznab:attr':{_attr:{name: 'minimumseedtime'	, value:'0.0'}}},
+							{'torznab:attr':{_attr:{name: 'size'			, value:torrent.size}}},
+							/*	]
+								}*/
+							]
 	};
 }
 
@@ -306,8 +321,8 @@ function callbackLoginT411 (error, response, body)
 		else
 		{
 			console.log("Failed to login to T411");
-		    console.log("Please verify your credentials in 'config.json'");
-	       	process.exit(-1);	       
+			console.log("Please verify your credentials in 'config.json'");
+			process.exit(-1);	       
 		}
 	}
 }
---
+++ tvmaze.js
@@ -0,0 +1,50 @@
+var util = require("util"),
+//    xml2js = require("xml2js"),
+    request = require("request"),
+//    parser = new xml2js.Parser({explicitArray: false}),
+    // All the URLs we will be making use of.
+    baseUrl = "http://api.tvmaze.com/",
+    showInfoUrl = "lookup/shows",
+    fullShowInfoUrl = "lookup/shows";
+
+// Responsible for sending a request down to the url that has
+// been passed as an argument.
+_request = function(url, callback) {
+    request({uri: url}, function(err, response, body) {
+        if (!err && response.statusCode == 200) {
+           var outJson= JSON.parse (body);
+	       	//parser.parseString(body, function(err, result) {
+                var output = {'Showinfo' :
+			{showname : outJson['name']}
+		};
+		//body['Showinfo']['showname'] = body.name;
+		callback(err,output);
+            //});
+        } else {
+            _httpError(err, response, callback);
+        }
+    });
+};
+
+// Responsible for raising an error with the appropriate
+// status code.
+_httpError = function(error, response, callback) {
+    var status = (response && response.statusCode) ? (response.statusCode) : (error.code);
+    callback(new Error(util.format("The TVRage API responded with status code %s", status)));
+};
+
+
+// Show info based on a show id that can be acquired via search
+// or fullSearch.
+exports.showInfoTvRage = function(showId, callback) {
+    var url = util.format("%s%s?tvrage=%s", baseUrl, showInfoUrl, showId);
+    _request(url, callback);
+};
+
+// Full show info based on a show id that can be acquired via search
+// or fullSearch.
+exports.fullShowInfoTvRage = function(showId, callback) {
+    var url = util.format("%s%s?tvrage=%s", baseUrl, fullShowInfoUrl, showId);
+    _request(url, callback);
+};
+
