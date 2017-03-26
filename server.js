e2ee8c410d96e7ca5050ee89db01467b0111027b
François-Xavier Payet <fx.payet@tfdn.org>
2017-03-26 12:10:22 +0200

    Passage en t411.ai

--- server.js
+++ server.js
@@ -33,8 +33,8 @@ var DEFAULT_LIMIT = 50;
 var TVRAGE_CACHE_MINS = 300; // 5Hours
 
 // System variables
-var baseUrl = "http://api.t411.li";
-var baset411 = "https://www.t411.li";
+var baseUrl = "http://api.t411.ai";
+var baset411 = "https://www.t411.ai";
 var userToken = ""; // Holds the user token for the T411 API
 
 var _T411_CatTVShow = {name:"Série TV", idCat : 0};
